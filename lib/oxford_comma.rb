079ed9dc1a8b94c9af7a641d405b47381a99ba1c
Dakota Wicker <dwicker1996@gmail.com>
2020-03-26 00:41:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.count = 1
   return array
 elsif array.count = 2
-  return.join(" and ")
+  return array.join(" and ")
 else arry[- 1] = "and #{array[-1]}"
   return array.join(",")
 end
