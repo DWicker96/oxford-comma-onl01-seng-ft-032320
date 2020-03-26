6d4d9084fd5b88eb5c2953efea4f456a7c092dc2
Dakota Wicker <dwicker1996@gmail.com>
2020-03-26 00:44:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,3 @@
 def oxford_comma(array)
-if array.count = 1
-  return array
-elsif array.count = 2
-  return array.join(" and ")
-else array[- 1] = "and #{array[-1]}"
-  return array.join(",")
-end
+
 end
